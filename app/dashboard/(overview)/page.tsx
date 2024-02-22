d28ad20a8357292d6b7909ccbbb2bdb393ddf1b4
shivkumarojha <ojhashivkumar88@gmail.com>
2024-02-22 17:13:25 +0530

    added create invoice route

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
     numberOfCustomers,
     numberOfInvoices,
     totalPaidInvoices,
-    totalPendingInvoices,
+    totalPendingInvoices
   } = await fetchCardData();
   return (
     <main>
